9701e905dfa37af4d2dfcdb58d4b55c7d4ac40b9
bchauhan4200@conestogac.on.ca <bchauhan4200@conestogac.on.ca>
2023-02-18 19:28:43 -0500

    Updated cart_get

--- public/api/cart_get.php
+++ public/api/cart_get.php
@@ -4,18 +4,18 @@ if(!INTERNAL){
     exit("Not Allowed Direct Acess");
 }
 //checks if thers already a SESSION in progress
-if( empty($_SESSION['cartId']) ){
-    print(json_encode([]));
-    exit();
-} else {
-    $cartId = intval($_SESSION['cartId']);
-}
+// if( empty($_SESSION['cartId']) ){
+//     print(json_encode([]));
+//     exit();
+// } else {
+//     $cartId = intval($_SESSION['cartId']);
+// }
 
 
 $query = "SELECT * FROM products 
 INNER JOIN cartItems ON products.id = cartItems.productID
 INNER JOIN cart ON cart.id = cartItems.cartID
-WHERE  cart.id = {$cartId}";
+WHERE  cart.id = 1";
 
 $result = mysqli_query($conn, $query);
 
@@ -25,7 +25,7 @@ while($row = mysqli_fetch_assoc($result)) {  // mysqli_fetch_assoc loops through
 }
 
 if($productData === []){
-    print("[]");
+    print(json_encode($productData));
     exit();
 } else {
     print(json_encode($productData));
